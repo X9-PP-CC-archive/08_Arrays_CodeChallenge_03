7cf6878828245e637661d9aea4bddd94b3db96f7
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-04-06 12:59:38 +0200

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,26 @@
+
+/*** Mini-Challenge  */
+// Satzbau + Punktuation
+
+// Vereinbarung:
+// "S" (sentence) --> "."  Ich bin Max.
+// "Q" (question) --> "?"  Bist du Max?
+// "E" (exclamation) --> "!"  Ich bin!
+
+// Parameter --> funktion getSentence([ ...], "Q"){ ...}
+
+
+/*** Funktion mit Array als Parameter */
+// mögliche Tests:
+output(getSentence(["Ich","bin","Max"],"S"));
+output(getSentence(["Bist","du","Max"],"Q"));
+output(getSentence(["Ich","bin"],"E"));
+output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+
+
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+function output(inputStr) {
+    console.log(inputStr);
+}
